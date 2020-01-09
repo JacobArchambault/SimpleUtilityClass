5bb4d168066bb26180afbc422186c1a61fb3cd31
Jacob Archambault <jacob.archambault@waystar.com>
2020-01-09 08:00:40 -0500

    Remove unused usings and args parameter.

--- SimpleUtilityClass/TimeUtilClass.cs
+++ SimpleUtilityClass/TimeUtilClass.cs
@@ -1,10 +1,5 @@
 ﻿using static System.Console;
 using static System.DateTime;
-using System.Globalization;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleUtilityClass
 {
